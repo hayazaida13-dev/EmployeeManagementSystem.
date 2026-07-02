d52f2bffc5576dbba35e0b7948be01a960661889
Haya Agniya Jaida Malik <hayazaida13@gmail.com>
2026-07-02 09:48:24 +0700

    Initial project setup with App.java

---
+++ src/controller/LoginController.java
@@ -0,0 +1,8 @@
+package controller;
+
+public class LoginController {
+
+    public boolean login(String username, String password) {
+        return username.equals("admin") && password.equals("admin");
+    }
+}
