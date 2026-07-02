7d94becaa945b972b4c0e607ca4ed0cb42fef01b
Haya Agniya Jaida Malik <hayazaida13@gmail.com>
2026-07-02 11:25:48 +0700

    Fix App.java main entry point

--- src/App.java
+++ src/App.java
@@ -1,11 +1,14 @@
+import controller.LoginController;
+import controller.EmployeeController;
+
 public class App {
     public static void main(String[] args) {
+
         LoginController login = new LoginController();
 
-if (login.login()) {
-    EmployeeController emp = new EmployeeController();
-    emp.menu();
-}  
-// TODO: imptove error handling for Login failure
+        if (login.login()) {
+            EmployeeController emp = new EmployeeController();
+            emp.menu();
+        }
     }
-}  
+}
