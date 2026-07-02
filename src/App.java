b754a4495b810a66682de6b6d3efd50574fd242a
Haya Agniya Jaida Malik <hayazaida13@gmail.com>
2026-07-02 11:17:14 +0700

    Add login flow documentation comments

--- src/App.java
+++ src/App.java
@@ -1,6 +1,11 @@
-LoginController login = new LoginController();
+public class App {
+    public static void main(String[] args) {
+        LoginController login = new LoginController();
 
 if (login.login()) {
     EmployeeController emp = new EmployeeController();
     emp.menu();
-}    
+}  
+// TODO: imptove error handling for Login failure
+    }
+}  
